102918fa62acaaf3d2daa323ee4b40ca66e64acb
António Cardoso <antoniocardosopessoal@gmail.com>
2023-10-16 22:35:39 +0100

    Clean up trash files

--- menu.js
+++
@@ -1,9 +0,0 @@
-
-function customBoardSize(){
-    let board_size_select = document.getElementById("board-size-select");
-    let custom_size_div = document.getElementById("custom-board-size");
-    if (board_size_select.options[board_size_select.selectedIndex].text == "Custom"){
-        custom_size_div.style.display = "flex";
-    }
-    else{custom_size_div.style.display = "none";}
-}
